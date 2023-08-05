2be6ea1a0b8dd1ca21cb22ff74172e7e94a8bc92
megha <megha.95patel@gmail.com>
2023-08-05 13:48:26 +0530

    first commit

---
+++ app/Http/Controllers/EmpAuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+use Hash;
+
+class EmpAuthController extends Controller
+{
+    public function register(){
+        return view('Emp.register');
+    }
+
+    public function create(Request $request){
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password),
+        ]);
+
+        return redirect('/dashboard')->withSuccess("SuccessFully Inserted!!!!");
+    }
+
+    public function dashboard(){
+        if(Auth::check()){
+            $user = Auth::user();
+            return view('Emp.dashboard',['user'=>$user]);
+        }
+
+        return redirect('/login');
+        
+    }
+
+    public function login(){
+        return view('Emp.login');
+    }
+
+    public function authemp(Request $request){
+        $data = $request->only('email','password');
+        if(Auth::attempt($data)){
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard')->withSuccess('Login Successfully');
+
+        }
+
+        return back()->withErrors('The provided credentials do not match our records.',);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\EmpAuthController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/register',[EmpAuthController::class,'register'])->name('register');
+Route::get('/dashboard',[EmpAuthController::class,'dashboard'])->name('dashboard');
+Route::get('/login',[EmpAuthController::class,'login'])->name('login');
+
+
+Route::post('/createemp',[EmpAuthController::class,'create'])->name('createemp');
+Route::post('/authemp',[EmpAuthController::class,'authemp'])->name('authemp');
